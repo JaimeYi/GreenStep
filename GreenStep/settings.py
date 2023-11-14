5da2802715aa1cc14fd7e9c7cd8df3ee88ec5a3a
Sxbayb <ssantander@usm.cl>
2023-11-13 21:12:39 -0300

    profile

--- GreenStep/settings.py
+++ GreenStep/settings.py
@@ -119,11 +119,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'GreenStep/static/'
--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Vehiculo
-
+from .models import Profile
 # Register your models here.
 
 admin.site.register(Vehiculo)
+admin.site.register(Profile)
